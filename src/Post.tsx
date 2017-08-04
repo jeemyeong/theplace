b85dac671984add445d7997cf2a2deb4be86e164
jeemyeong <jeemyeong@gmail.com>
2017-08-05 01:55:08 +0900

    Make Text in Photo

--- src/Post.tsx
+++ src/Post.tsx
@@ -12,12 +12,42 @@ export interface PostProps {
 class Post extends React.Component<PostProps, {}> {
   render() {
     const { state } = this.props.postStore as PostStore;
+    const src = !!state ? state.src : '';
     return (
-      <div>
-        <Image src={state.src} size="large" />
+      <div style={containerStyle}>
+        <h2 style={textStyle}>
+          <span style={spanStyle}>
+            Text
+          </span>
+        </h2>
+        <Image src={src} style={cardImageStyle} size="large"/>
       </div>
     );
   }
 }
+
+const cardImageStyle = {
+  width: '100%',
+  position: 'relative' as React.CSSWideKeyword
+};
+
+const containerStyle = {
+};
+
+const textStyle = {
+  zIndex: 100,
+  position: 'absolute' as React.CSSWideKeyword,
+  color: 'white',
+  left: 0,
+  top: 200
+};
+
+const spanStyle = {
+   color: 'white',
+   font: 'bold 24px/45px Helvetica, Sans-Serif',
+   letterSpacing: '-1px',
+   background: 'rgba(0, 0, 0, 0.7)',
+   padding: '10px'
+};
 
 export default Post;

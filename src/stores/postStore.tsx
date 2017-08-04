1c82a59e0aec5779af70dcaf97b4e5118f2939a7
jeemyeong <jeemyeong@gmail.com>
2017-08-05 01:55:01 +0900

    Modify src for food

--- src/stores/postStore.tsx
+++ src/stores/postStore.tsx
@@ -3,7 +3,7 @@ import { observable } from 'mobx';
 export class PostStore {
   @observable
   state = {
-    src: 'https://react.semantic-ui.com/assets/images/wireframe/image.png'
+    src: 'https://photos-1.dropbox.com/t/2/AAD475_Ezp6ePoaJK4g2TL7R9in_P8m7SvEdyy86exmstA/12/343512610/png/32x32/3/1501880400/0/2/1_pizza.PNG/EPPZ4tcCGI3lDSAHKAc/P7v0yz-WZIwe15EofJMT8dYrDbQ8ufNbC2RtDXrQyhk?dl=0&size=2048x1536&size_mode=3'
   };
 }
 

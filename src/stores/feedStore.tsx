17122b19989d6e82fa65dce876bd6c40eb9da855
jeemyeong <jeemyeong@gmail.com>
2017-08-08 23:40:14 +0900

    Modify store imgs

--- src/stores/feedStore.tsx
+++ src/stores/feedStore.tsx
@@ -38,8 +38,124 @@ export class FeedStore {
       },
       {
         imgUrlArray: [
-          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/20066942_475308786169319_2365934253134315520_n.jpg',
-          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/20180684_132905990635793_1117704709809897472_n.jpg'
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/19984681_824326591102559_1784158607651110912_n.jpg?se=7'
+        ],
+        author: {
+          id: 0,
+          nickname: 'jeemyeong',
+          profileImgUrl: 'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-19/s150x150/19050682_812811472214414_8464831635005636608_a.jpg',
+        },
+        restaurant: '일마지오',
+        review: '맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어',
+        evaluate: 4.5
+      },
+      {
+        imgUrlArray: [
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/18949561_1829440647383473_5320020906876076032_n.jpg?se=7'
+        ],
+        author: {
+          id: 0,
+          nickname: 'jeemyeong',
+          profileImgUrl: 'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-19/s150x150/19050682_812811472214414_8464831635005636608_a.jpg',
+        },
+        restaurant: '일마지오',
+        review: '맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어',
+        evaluate: 4.5
+      },
+      {
+        imgUrlArray: [
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/18646328_322514511502422_1401828856708464640_n.jpg?se=7'
+        ],
+        author: {
+          id: 0,
+          nickname: 'jeemyeong',
+          profileImgUrl: 'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-19/s150x150/19050682_812811472214414_8464831635005636608_a.jpg',
+        },
+        restaurant: '잇쇼우',
+        review: '그냥그랬어',
+        evaluate: 3.5
+      },
+      {
+        imgUrlArray: [
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/18723076_301756766916358_1785453106499158016_n.jpg?se=7'
+        ],
+        author: {
+          id: 0,
+          nickname: 'jeemyeong',
+          profileImgUrl: 'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-19/s150x150/19050682_812811472214414_8464831635005636608_a.jpg',
+        },
+        restaurant: '일마지오',
+        review: '맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어',
+        evaluate: 4.5
+      },
+      {
+        imgUrlArray: [
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/18646675_954891047985830_1372512861644914688_n.jpg?se=7',
+        ],
+        author: {
+          id: 0,
+          nickname: 'jeemyeong',
+          profileImgUrl: 'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-19/s150x150/19050682_812811472214414_8464831635005636608_a.jpg',
+        },
+        restaurant: '일마지오',
+        review: '맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어',
+        evaluate: 4.5
+      },
+      {
+        imgUrlArray: [
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/18381036_1865460683741496_1535604460775014400_n.jpg?se=7'
+        ],
+        author: {
+          id: 0,
+          nickname: 'jeemyeong',
+          profileImgUrl: 'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-19/s150x150/19050682_812811472214414_8464831635005636608_a.jpg',
+        },
+        restaurant: '잇쇼우',
+        review: '그냥그랬어',
+        evaluate: 3.5
+      },
+      {
+        imgUrlArray: [
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/18162170_1770929596551664_2114254753046200320_n.jpg?se=7'
+        ],
+        author: {
+          id: 0,
+          nickname: 'jeemyeong',
+          profileImgUrl: 'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-19/s150x150/19050682_812811472214414_8464831635005636608_a.jpg',
+        },
+        restaurant: '일마지오',
+        review: '맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어',
+        evaluate: 4.5
+      },
+      {
+        imgUrlArray: [
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/18096349_1975915102640619_4434855136744964096_n.jpg?se=7'
+        ],
+        author: {
+          id: 0,
+          nickname: 'jeemyeong',
+          profileImgUrl: 'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-19/s150x150/19050682_812811472214414_8464831635005636608_a.jpg',
+        },
+        restaurant: '일마지오',
+        review: '맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어맛있어',
+        evaluate: 4.5
+      },
+      {
+        imgUrlArray: [
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/18094521_122807441603181_6865422398506663936_n.jpg?se=7'
+        ],
+        author: {
+          id: 0,
+          nickname: 'jeemyeong',
+          profileImgUrl: 'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-19/s150x150/19050682_812811472214414_8464831635005636608_a.jpg',
+        },
+        restaurant: '잇쇼우',
+        review: '그냥그랬어',
+        evaluate: 3.5
+      },
+      {
+        imgUrlArray: [
+          'https://instagram.ficn1-1.fna.fbcdn.net/t51.2885-15/e35/17596057_1394325537305920_8161715761199448064_n.jpg?se=7'
         ],
         author: {
           id: 0,

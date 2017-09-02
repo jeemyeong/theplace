9e0d0fafc5ee1532f7c49c1bd0c443af2b9070bd
jeemyeong <jeemyeong@gmail.com>
2017-09-02 17:41:53 +0900

    Split remove data and init seed data

---
+++ src/database/removeAllData.ts
@@ -0,0 +1,9 @@
+import { database } from './database';
+
+const removeAllReviews = async () => {
+  const databaseRef = database.ref()
+  await databaseRef.remove()
+  process.exit()
+}
+
+removeAllReviews()

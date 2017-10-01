4a5789be76acde3856c980f0b3271a8fec047d69
jeemyeong <jeemyeong@gmail.com>
2017-10-02 00:41:11 +0900

    Add facebook Img

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -3,6 +3,7 @@ import { Image } from 'semantic-ui-react'
 import { observer } from 'mobx-react';
 import { style } from 'typestyle';
 import * as csstips from 'csstips';
+const facebookImg = require('./facebook.png');
 
 const AuthStyle = style(csstips.fillParent, csstips.flexRoot, {
 });
@@ -22,7 +23,7 @@ class Auth extends React.Component<AuthProps, {}> {
         <Image
           className={LoginImageStyle}
           onClick={this.props.loginWithFacebook}
-          src="https://scontent.ficn1-1.fna.fbcdn.net/v/t39.2365-6/17639236_1785253958471956_282550797298827264_n.png?oh=3f070b94fe0c2a2814582e91e950d6f8&oe=5A1358EA"
+          src={facebookImg}
         />
       </div>
     );
